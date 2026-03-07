1c5a7b912cfaf447834787be27da9f3794f36f60
glenn <wuglenn.wg@gmail.com>
2026-03-07 21:08:01 +0800

    targetting vercel build

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["mapbox-gl"],
-  turbopack: {
-    root: __dirname,
-  },
 };
 
 export default nextConfig;
